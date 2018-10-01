563e9f0e932a7c54ba0d673b6c4a57828ce9f123
Rodrigo Ramirez <user.name>
2018-10-01 17:49:06 -0600

    Added code for localstorage

--- pokemon-stats/src/index.js
+++ pokemon-stats/src/index.js
@@ -7,8 +7,33 @@ import { Provider } from 'react-redux';
 
 
 import postReducer from './reducers/postReducer';
-const store = createStore(postReducer);
+function saveToLocalStorage(state) {
+    try {
+      const serializedState = JSON.stringify(state)
+      localStorage.setItem('state', serializedState)
+    } catch(e) {
+      console.log(e)
+    }
+  }
+  
+  function loadFromLocalStorage() {
+    try {
+      const serializedState = localStorage.getItem('state')
+      if (serializedState === null) return undefined
+      return JSON.parse(serializedState)
+    } catch(e) {
+      console.log(e)
+      return undefined
+    }
+  }
+  
+  const persistedState = loadFromLocalStorage()
+  
+  const store = createStore(postReducer, persistedState);
+  
+  store.subscribe(() => saveToLocalStorage(store.getState()))
 ReactDOM.render(
 <Provider store={store}>
 <App />
 </Provider>, document.getElementById('root'));
+
